af67745735d93cf042b89ce0271e8ef65fa88e6a
enzoaravenaq <enzoaravenaq@gmail.com>
2020-11-13 15:53:04 -0300

    App.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,21 +4,21 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-import Vuex from 'vuex'
-Vue.use(Vuex)
+//import Vuex from 'vuex'
+//Vue.use(Vuex)
 
-import storeData from "./store/index"
+//import storeData from "./store/index"
 
-const store = new Vuex.Store(
-    storeData
-)
+//const store = new Vuex.Store(
+//    storeData
+//)
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('order-test-levels-component', require('./components/OrderTestLevelsComponent.vue').default);
-Vue.component('level-execution-global-component', require('./components/LevelExecutionGlobalComponent.vue').default);
-Vue.component('level-execution-activities-component', require('./components/LevelExecutionActivitiesComponent.vue').default);
+//Vue.component('level-execution-global-component', require('./components/LevelExecutionGlobalComponent.vue').default);
+//Vue.component('level-execution-activities-component', require('./components/LevelExecutionActivitiesComponent.vue').default);
 Vue.component('activity-result-form-component', require('./components/ActivityResultFormComponent.vue').default);
 Vue.component('activity-result-edit-form-component', require('./components/ActivityResultEditFormComponent.vue').default);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
@@ -26,7 +26,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
-    store
 });
 
 
